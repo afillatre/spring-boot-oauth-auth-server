2b83196a5a6fc3f8b9ed41bbbf6ca6c60953962f
afillatre <afillatre@ippon.fr>
2017-01-05 17:30:48 +0100

    Fix auth strategies

--- src/main/java/com/cdiscount/poc/oauth/controller/AuthorizationController.java
+++ src/main/java/com/cdiscount/poc/oauth/controller/AuthorizationController.java
@@ -3,7 +3,6 @@ package com.cdiscount.poc.oauth.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -14,11 +13,6 @@ import java.util.Map;
  */
 @RestController
 public class AuthorizationController {
-
-    @RequestMapping("/code")
-    public String getCode(HttpServletRequest request) {
-        return request.getParameter("code");
-    }
 
     @RequestMapping({ "/user", "/me" })
     public Map<String, String> user(Principal principal) {
